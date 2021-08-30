a800a80b2d8989782dcd29764ffc28725eca92dd
isahaksahakian <sahakianis@yahoo.com>
2021-08-30 14:54:12 +0400

    add WordIsOnlyDigit

---
+++ src/main/java/com/sahakian/string/WordIsOnlyDigit.java
@@ -0,0 +1,29 @@
+package com.sahakian.string;
+
+import java.util.Arrays;
+
+public class WordIsOnlyDigit {
+    public static void main(String[] args) {
+        String word = "1234";
+
+        System.out.println(wordContainsOnlyDigits1(word));
+        System.out.println(wordContainsOnlyDigits2(word));
+    }
+
+    private static boolean wordContainsOnlyDigits1(String word){
+        char[] charArray = word.toCharArray();
+
+        for (char c : charArray) {
+            if (!Character.isDigit(c)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean wordContainsOnlyDigits2(String word){
+        return word
+                .chars()
+                .allMatch(value -> Character.isDigit((char) value));
+    }
+}
