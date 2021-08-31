cee8d2ef23c371f065bdc0f4eea2813dca18b434
isahaksahakian <sahakianis@yahoo.com>
2021-08-31 22:08:28 +0400

    add ContainsSubString

---
+++ src/main/java/com/sahakian/string/ContainsSubString.java
@@ -0,0 +1,11 @@
+package com.sahakian.string;
+
+public class ContainsSubString {
+    public static void main(String[] args) {
+
+    }
+
+    private static boolean contains(String word,String subText){
+        return word.contains(subText);
+    }
+}
